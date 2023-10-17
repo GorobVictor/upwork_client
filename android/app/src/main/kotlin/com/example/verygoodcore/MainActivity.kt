70b91383a611ad55bf37af83d662f093871eadc8
GorobVictor <Gorobchuk333@gmail.com>
2023-10-18 00:40:57 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.upwork_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:upwork_client/app/app.dart';
+import 'package:upwork_client/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
